93dd6c98fd925d89eb23901d32829e59af330882
pbsmith82 <pbsmith82@gmail.com>
2021-02-10 00:48:09 -0500

    commit tree 732cf9856f01bdaa888ede7ddca5ced8673bd6e2

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+    def new
+     
+    end
+    
+    def create
+      @user = User.find_by(name: params[:user][:name])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+      else
+        redirect_to '/login'
+      end 
+    end
+    
+      def destroy
+       session.clear
+       redirect_to login_path
+      end
+    
+end
